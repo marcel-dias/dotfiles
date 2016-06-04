a9f161b36b34b6205a7d69aa9017bc968d5d983f
Marcel Dias <marceldiass@gmail.com>
2016-06-04 16:35:27 -0300

    remove command-t setup

--- vim/install.sh
+++ vim/install.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-setup_command_t() {
-  cd "$ZSH"/vim/vimfiles/vim.symlink/bundle/command-t/ruby/command-t
-  ruby extconf.rb
-  make
-}
 [ "$(uname -s)" = "Darwin" ] && brew install vim
 vim +PluginInstall +qall >/tmp/vim-plugins.log 2>&1 || true
-setup_command_t >/tmp/cmd-t.log 2>&1 || true
