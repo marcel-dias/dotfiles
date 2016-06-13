640118328fe0cce6b77159be5eeb85c5ab5fdadd
Marcel Dias <marceldiass@gmail.com>
2016-06-12 23:18:09 -0300

    Remove unused aliases and add grh

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,8 +2,6 @@
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias glg="git log --graph --decorate --oneline --abbrev-commit"
-alias gp='git push origin HEAD'
-alias gpa='git push origin --all'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
@@ -11,15 +9,11 @@ alias gco='git checkout'
 alias gb='git branch'
 alias ga='git add'
 alias gaa='git add -A'
-alias gcm='git commit -m'
-alias gcam='git commit -a -m'
+alias gcm='git commit -S -m'
 alias gs='git status -sb'
 alias gcb='git-copy-branch-name'
 alias gst='git status'
-
-gpr() {
-  gp && open-pr "$*"
-}
+alias grh='git reset HEAD'
 
 gi() {
   curl -s "https://www.gitignore.io/api/$*";
