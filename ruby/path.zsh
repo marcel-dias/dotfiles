5ed9007817c4289766088c38e1830abcb2f181a2
Marcel Dias <marceldiass@gmail.com>
2016-09-21 13:25:29 -0300

    Delete ruby stuff

--- ruby/path.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Add RVM to PATH for scripting
-export PATH="$PATH:$HOME/.rvm/bin"
-
-# Load RVM into a shell session *as a function*
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
