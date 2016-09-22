326ad501dc20b5a8b115528e211ed14e06ce9b77
Marcel Dias <marceldiass@gmail.com>
2016-09-22 18:38:10 -0300

    Remove terminal config

--- terminal/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ "$(uname -s)" = "Darwin" ]; then
-  cp com.apple.Terminal.plist.`sw_vers | grep 'ProductVersion:' | grep -o '10\.[0-9]*'` \
-    ~/Library/Preferences/com.apple.Terminal.plist \
-    && defaults read com.apple.Terminal
-fi
