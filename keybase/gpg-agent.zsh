875fedf6ddcaef2e7f92a2f0a0d5ef44cfc8a4bf
Marcel Dias <marceldiass@gmail.com>
2016-09-22 18:21:59 -0300

    Update keybase and gpg stuff

--- keybase/gpg-agent.zsh
+++ keybase/gpg-agent.zsh
@@ -6,13 +6,3 @@ else
   eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
 fi
 
-# add the use-agent line to ~/.gnupg/gpg.conf
-GPG_USE_AGENT=`grep -e "#use-agent" ~/.gnupg/gpg.conf`
-if [ "x$GPG_USE_AGENT" != "x" ]; then
-  echo "use-agent" >> ~/.gnupg/gpg.conf
-fi
-
-if [ ! -f ~/.gnupg/gpg-agent.conf ]; then
-  touch ~/.gnupg/gpg-agent.conf
-  echo "use-standard-socket\npinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
-fi
