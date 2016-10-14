eb4a4cce90bb836f4f6c1aec8c4cdd8f713d87fa
Marcel Dias <marceldiass@gmail.com>
2016-10-13 23:08:35 -0300

    Ask for sudo earlier

--- macOS/set-defaults.sh
+++ macOS/set-defaults.sh
@@ -13,6 +13,12 @@ if [ "$(uname -s)" != "Darwin" ]; then
   exit 0
 fi
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `set-defaults.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
