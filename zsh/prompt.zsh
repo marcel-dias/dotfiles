36741341691bd7ce8a44e15ce189d7bec64ee7a8
Marcel Dias <marceldiass@gmail.com>
2016-10-13 23:10:57 -0300

    Load antibody bundles from .antibody_bundles.txt file

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,8 +1,2 @@
 #!/bin/zsh
 PURE_CMD_MAX_EXEC_TIME=1
-antibody bundle mafredri/zsh-async
-antibody bundle sindresorhus/pure
-
-# these two guys **MUST** be loaded last.
-antibody bundle zsh-users/zsh-history-substring-search
-antibody bundle zsh-users/zsh-syntax-highlighting
