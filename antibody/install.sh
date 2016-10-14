63c1b1e45183a15e5fc13fc65974c948938c538a
Marcel Dias <marceldiass@gmail.com>
2016-10-13 23:09:31 -0300

    Rename antibody bundles files

--- antibody/install.sh
+++ antibody/install.sh
@@ -2,6 +2,6 @@
 if [ "$(uname -s)" != "Darwin" ]; then
   exit 0
 fi
-antibody bundle < "$ZSH/antibody/bundles.txt" > ~/.bundles.txt
-antibody bundle sindresorhus/pure >> ~/.bundles.txt
-antibody bundle < "$ZSH/antibody/bundle_zsh.txt" >> ~/.bundles.txt
+antibody bundle < "$ZSH/antibody/bundles.txt" > ~/.antibody_bundles.txt
+antibody bundle sindresorhus/pure >> ~/.antibody_bundles.txt
+antibody bundle < "$ZSH/antibody/bundle_zsh.txt" >> ~/.antibody_bundles.txt
