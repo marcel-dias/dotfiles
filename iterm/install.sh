2786f5225dcdb584ff907faaba8e1b7b45b85484
Marcel Dias <marceldiass@gmail.com>
2016-12-26 18:12:10 -0200

    Fix sed command to replace paths

--- iterm/install.sh
+++ iterm/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 [ "$(uname -s)" != "Darwin" ] && exit 0
-sed "s;/Users/$USERNAME/.dotfiles;$ZSH;" \
-  "$ZSH"/iterm/com.googlecode.iterm2.plist.example > "$ZSH"/iterm/com.googlecode.iterm2.plist
+sed -e "s;/Users/USERNAME/.dotfiles;$ZSH;" \
+    -e "s;/Users/USERNAME;/Users/$USERNAME;" \
+    "$ZSH"/iterm/com.googlecode.iterm2.plist.example > "$ZSH"/iterm/com.googlecode.iterm2.plist
 defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$ZSH/iterm"
 defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
