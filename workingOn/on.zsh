df6125f869a9af330212449b41599956fa025e52
Marcel Dias <marceldiass@gmail.com>
2017-01-10 17:49:38 -0200

    Fix shellcheck issue

--- workingOn/on.zsh
+++ workingOn/on.zsh
@@ -1,4 +1,5 @@
 #!/bin/sh
 on() {
-  noglob curl -X POST --data-urlencode "task=$*" https://api.workingon.co/hooks/incoming?token=$WORKINGON_TOKEN >/dev/null 2>&1 echo "Task sent to WorkingOn."
+  curl -X POST --data-urlencode "task=$*" "https://api.workingon.co/hooks/incoming?token=$WORKINGON_TOKEN" >/dev/null 2>&1
+  echo "Task sent to WorkingOn."
 }
