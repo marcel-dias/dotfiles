59eb2d324c5d6de0c3e15c4d632528b158c73db9
Marcel Dias <marceldiass@gmail.com>
2017-04-05 17:48:43 -0300

    Remove use-standart-socket since in gpg-agent (GnuPG) 2.1.19 it is deprecated

--- keybase/install.sh
+++ keybase/install.sh
@@ -7,7 +7,6 @@ fi
 
 if [ ! -f ~/.gnupg/gpg-agent.conf ]; then
   touch ~/.gnupg/gpg-agent.conf
-  echo "use-standard-socket" >> ~/.gnupg/gpg-agent.conf
   echo "default-cache-ttl 3600" >> ~/.gnupg/gpg-agent.conf
   echo "max-cache-ttl 3600" >> ~/.gnupg/gpg-agent.conf
   echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
