2df2fd61e94e6d817595b17a6ffca795df8fcff8
Marcel Dias <marceldiass@gmail.com>
2017-05-02 07:33:14 -0300

    Remove --write-env-file argument

--- keybase/gpg-agent.zsh
+++ keybase/gpg-agent.zsh
@@ -3,6 +3,5 @@ if test -f ~/.gnupg/.gpg-agent-info -a -n "$(pgrep gpg-agent)"; then
   source ~/.gnupg/.gpg-agent-info
   export GPG_AGENT_INFO
 else
-  eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+  eval $(gpg-agent --daemon ~/.gnupg/.gpg-agent-info)
 fi
-
