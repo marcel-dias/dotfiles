a613ffaca605d3d71a803c99dc5fc5e147f6e0e1
Marcel Dias <marceldiass@gmail.com>
2017-09-07 23:15:04 -0300

    change alias from code to src to be able to use code as Visual Studio Code. rm ca. update drmi to use dangling=true sintax
    Signed-off-by: Marcel Dias <marceldiass@gmail.com>

--- user/aliases.zsh
+++ user/aliases.zsh
@@ -1,10 +1,9 @@
 #!/bin/sh
-alias code='cd ~/Code'
+alias src='cd ~/Code'
 alias blog='cd ~/Code/marceldiass.github.io'
 alias wildfly='cd /Applications/wildfly'
-alias ca='cd ~/Code/contaazul'
 alias dps='docker ps -a'
 alias dm='docker $(docker-machine config)'
 alias dmps='docker $(docker-machine config) ps -a'
 alias dms='docker-machine start'
-alias drmi='docker rmi $(docker images -a -q)'
+alias drmi='docker rmi $(docker images -f "dangling=true" -q )'
