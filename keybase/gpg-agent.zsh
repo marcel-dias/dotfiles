b47fb63ba01ec9bfc1131c39126468090d4f2c1d
Marcel Dias <marceldiass@gmail.com>
2017-10-09 11:18:16 -0400

    fix gpg agent

--- keybase/gpg-agent.zsh
+++ keybase/gpg-agent.zsh
@@ -1,7 +1,6 @@
 # gpg-agent
-if test -f ~/.gnupg/.gpg-agent-info -a -n "$(pgrep gpg-agent)"; then
-  source ~/.gnupg/.gpg-agent-info
-  export GPG_AGENT_INFO
+if test -n "$(pgrep gpg-agent)"; then
+
 else
   eval $(gpg-agent --daemon)
 fi
