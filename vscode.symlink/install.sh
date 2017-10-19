03219056dc921bebeb43220e36a1078d4161a146
Marcel Dias <marceldiass@gmail.com>
2017-10-19 13:35:48 -0400

    Add Visual Code configurations

---
+++ vscode.symlink/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ "$(uname -s)" != "Darwin" ]; then
+  exit 0
+fi
+
+if test "$(which code)"; then
+    code --install-extension webstp.extension-manager
+    code --install-extension DavidAnson.vscode-markdownlint
+    code --install-extension PeterJausovec.vscode-docker
+    code --install-extension dracula-theme.theme-dracula
+    code --install-extension ms-vscode.azure-account
+    code --install-extension redhat.java
+    code --install-extension vscjava.vscode-java-debug
+    code --install-extension vscjava.vscode-java-pack
+fi
