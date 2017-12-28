550ae1cebc0a31772295ec050ab5a0a4fa8b5d66
Marcel Dias <marceldiass@gmail.com>
2017-12-28 14:16:46 -0200

    Move docker aliases to dedicated file

---
+++ docker/aliases.zsh
@@ -0,0 +1,8 @@
+#!/bin/sh
+alias dps='docker ps -a'
+alias dm='docker $(docker-machine config)'
+alias dmps='docker $(docker-machine config) ps -a'
+alias dms='docker-machine start'
+alias drmi='docker rmi $(docker images -f "dangling=true" -q )'
+alias dri='docker rmi'
+alias di='docker images'
--- user/aliases.zsh
+++ user/aliases.zsh
@@ -2,8 +2,3 @@
 alias src='cd ~/Code'
 alias blog='cd ~/Code/marceldiass.github.io'
 alias wildfly='cd /Applications/wildfly'
-alias dps='docker ps -a'
-alias dm='docker $(docker-machine config)'
-alias dmps='docker $(docker-machine config) ps -a'
-alias dms='docker-machine start'
-alias drmi='docker rmi $(docker images -f "dangling=true" -q )'
