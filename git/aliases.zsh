30ba051a70a8dfa4afebbfecce51eef0128fce80
Marcel Dias <marceldiass@gmail.com>
2017-12-28 14:16:11 -0200

    Modify git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,7 +3,7 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias glg="git log --graph --decorate --oneline --abbrev-commit"
 alias gd='git diff'
-alias gc='git commit'
+alias gc='git clone'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
@@ -16,6 +16,8 @@ alias gst='git status'
 alias grh='git reset HEAD'
 alias gfu='git fetch upstream'
 alias gusub='git submodule update --recursive --remote'
+alias gp='git push'
+alias gpo='git-promote'
 
 gi() {
   curl -s "https://www.gitignore.io/api/$*";
