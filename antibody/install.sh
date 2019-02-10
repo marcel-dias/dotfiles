7e36a53d62eac37c3e6e46975bc9be8ba36de50c
Marcel Dias <marceldiass@gmail.com>
2019-02-10 18:35:49 -0200

    update antibody bundles

--- antibody/install.sh
+++ antibody/install.sh
@@ -3,5 +3,3 @@ if [ "$(uname -s)" != "Darwin" ]; then
   exit 0
 fi
 antibody bundle < "$ZSH/antibody/bundles.txt" > ~/.antibody_bundles.txt
-antibody bundle sindresorhus/pure >> ~/.antibody_bundles.txt
-antibody bundle < "$ZSH/antibody/bundle_zsh.txt" >> ~/.antibody_bundles.txt
