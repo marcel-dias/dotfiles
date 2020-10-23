413086f129a571e507661c53492feeb3beb48209
Marcel Dias <marceldiass@gmail.com>
2020-10-22 22:41:27 -0300

    improve keybase import key scripts

--- keybase/import_keys.sh
+++ keybase/import_keys.sh
@@ -3,10 +3,8 @@ keybase login
 keybase pgp pull $(keybase whoami)
 
 KEYBASE_KEY_ID=$(keybase pgp list | sed -n -e 's/^.*ID:  //p')
-keybase pgp export -q $KEYBASE_KEY_ID -o ~/keybase.public.key
-keybase pgp export -q $KEYBASE_KEY_ID --secret -o ~/keybase.secret.key
-gpg --import ~/keybase.public.key
-gpg --import ~/keybase.secret.key
+keybase pgp export -q $KEYBASE_KEY_ID | gpg --import
+keybase pgp export -q $KEYBASE_KEY_ID --secret | gpg --import --allow-secret-key-import
 
 # If you need to add your git email to your gpg
 #gpg --edit-key {username}@keybase.io
@@ -20,5 +18,3 @@ gpg --import ~/keybase.secret.key
 git config --global user.signingkey $(git config --get user.email)
 git config --global commit.gpgsign true
 git config --global gpg.program $(which gpg)
-
-rm ~/keybase.public.key ~/keybase.secret.key
