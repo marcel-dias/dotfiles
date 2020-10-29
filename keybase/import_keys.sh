96c4cacbec2f93dded88cbd3fce8bc6bd164ec89
Marcel Dias <marceldiass@gmail.com>
2020-10-28 21:05:39 -0300

    Fix shellchekc issues. improve keybase install key script.

--- keybase/import_keys.sh
+++ keybase/import_keys.sh
@@ -7,11 +7,11 @@ keybase pgp export -q $KEYBASE_KEY_ID | gpg --import
 keybase pgp export -q $KEYBASE_KEY_ID --secret | gpg --import --allow-secret-key-import
 
 # If you need to add your git email to your gpg
-#gpg --edit-key {username}@keybase.io
+#gpg --edit-key $(keybase whoami)@keybase.io
 # then adduid
 
-# Update the trust level to don't show warning messages
-# gpg --edit-key {username}@keybase.io trust quit
+echo "Update the trust level to don't show warning messages"
+gpg --edit-key $(keybase whoami)@keybase.io trust quit
 
 # keybase pgp update
 
