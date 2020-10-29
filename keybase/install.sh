867818db056881cfadd52e1acbda7c9bd3a88b56
Marcel Dias <marceldiass@gmail.com>
2020-10-28 21:05:39 -0300

    Fix shellchekc issues. improve keybase install key script.

--- keybase/install.sh
+++ keybase/install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+
+if [ ! -f ~/.gnupg/gpg.conf ]; then
+  touch ~/.gnupg/gpg.conf
+fi
+
 # add the use-agent line to ~/.gnupg/gpg.conf
-GPG_USE_AGENT=`grep -e "#use-agent" ~/.gnupg/gpg.conf`
+GPG_USE_AGENT=$(grep -e "#use-agent" ~/.gnupg/gpg.conf)
 if [ "x$GPG_USE_AGENT" != "x" ]; then
   echo "use-agent" >> ~/.gnupg/gpg.conf
 fi
@@ -8,7 +13,9 @@ fi
 if [ ! -f ~/.gnupg/gpg-agent.conf ]; then
   touch ~/.gnupg/gpg-agent.conf
 
-  echo "default-cache-ttl 3600" >> ~/.gnupg/gpg-agent.conf
-  echo "max-cache-ttl 3600" >> ~/.gnupg/gpg-agent.conf
-  echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
+  {
+    echo "default-cache-ttl 3600"
+    echo "max-cache-ttl 3600"
+    echo "pinentry-program /usr/local/bin/pinentry-mac"
+  } >> ~/.gnupg/gpg-agent.conf
 fi
