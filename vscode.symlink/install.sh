377da7e247ef24e3d3ffb4d53cfbed20fff707b8
Marcel Dias <marceldiass@gmail.com>
2020-11-04 21:18:20 -0300

    Update vscode settings

--- vscode.symlink/install.sh
+++ vscode.symlink/install.sh
@@ -3,6 +3,14 @@ if [ "$(uname -s)" != "Darwin" ]; then
   exit 0
 fi
 
+if [ ! -f ~/Library/Application\ Support/Code/User/settings.json ]; then
+  ln -s ~/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+else
+  rm ~/Library/Application\ Support/Code/User/settings.json
+  ln -s ~/.vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+fi
+
+
 if test "$(which code)"; then
   code --install-extension cssho.vscode-svgviewer
   code --install-extension hashicorp.terraform
@@ -38,3 +46,4 @@ if test "$(which code)"; then
   code --install-extension mathiasfrohlich.kotlin
   code --install-extension golang.go
 fi
+
