38eeb5db84af23948f4711038ea4c2b155c0483d
Marcel Dias <marceldiass@gmail.com>
2021-02-08 15:31:18 +0100

    Add armory arm alias to verify dinghyfiles

--- user/aliases.zsh
+++ user/aliases.zsh
@@ -2,3 +2,4 @@
 alias src='cd ~/Code'
 alias blog='cd ~/Code/marceldiass.github.io'
 alias lg='lazygit'
+alias arm="docker run --rm -it -v ${PWD}:/home/spinnaker armory/arm-cli arm dinghy render dinghyfile"
