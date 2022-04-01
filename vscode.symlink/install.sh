b19af640d1f26229d976a9fa91473f37bf47eaa1
Marcel Dias <marceldiass@gmail.com>
2022-04-01 11:18:20 +0200

    Update vscode plugins

--- vscode.symlink/install.sh
+++ vscode.symlink/install.sh
@@ -13,22 +13,25 @@ fi
 
 if test "$(which code)"; then
   code --install-extension cssho.vscode-svgviewer
-  code --install-extension hashicorp.terraform
   code --install-extension DavidAnson.vscode-markdownlint
   code --install-extension dracula-theme.theme-dracula
   code --install-extension eamodio.gitlens
   code --install-extension EditorConfig.EditorConfig
+  code --install-extension golang.go
+  code --install-extension hashicorp.terraform
   code --install-extension hediet.vscode-drawio
   code --install-extension jmMeessen.jenkins-declarative-support
-  code --install-extension mauve.terraform
   code --install-extension ms-azure-devops.azure-pipelines
   code --install-extension ms-azuretools.vscode-docker
   code --install-extension ms-python.python
+  code --install-extension ms-python.vscode-pylance
   code --install-extension ms-vscode-remote.remote-containers
   code --install-extension redhat.java
+  code --install-extension redhat.vscode-commons
   code --install-extension redhat.vscode-xml
   code --install-extension redhat.vscode-yaml
   code --install-extension RescueTime.rescuetime
+  code --install-extension sonarsource.sonarlint-vscode
   code --install-extension VisualStudioExptTeam.vscodeintellicode
   code --install-extension vscjava.vscode-java-debug
   code --install-extension vscjava.vscode-java-dependency
@@ -42,8 +45,6 @@ if test "$(which code)"; then
   code --install-extension wholroyd.HCL
   code --install-extension wholroyd.jinja
   code --install-extension xyz.local-history
-  code --install-extension sonarsource.sonarlint-vscode
-  code --install-extension mathiasfrohlich.kotlin
-  code --install-extension golang.go
 fi
+
 
