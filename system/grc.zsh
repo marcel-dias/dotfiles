103f69ff3a7ec7c9eb1f0ad625a0f0c5ad7cf2f3
Marcel Dias <marceldiass@gmail.com>
2022-04-01 15:02:09 +0200

    Fix grc config

--- system/grc.zsh
+++ system/grc.zsh
@@ -2,5 +2,5 @@
 # GRC colorizes nifty unix tools all over the place
 if which grc >/dev/null 2>&1 && which brew >/dev/null 2>&1; then
   # shellcheck disable=SC1090
-  . "$(brew --prefix)/etc/grc.bashrc"
+  . "$(brew --prefix)/etc/grc.zsh"
 fi
