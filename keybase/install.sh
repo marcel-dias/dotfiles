66303428ec8634597540c5546edf2bbfe280510e
Marcel Dias <marceldiass@gmail.com>
2022-10-01 19:05:38 -0400

    Update git sign steps

--- keybase/install.sh
+++ keybase/install.sh
@@ -6,16 +6,16 @@ fi
 
 # add the use-agent line to ~/.gnupg/gpg.conf
 GPG_USE_AGENT=$(grep -e "use-agent" ~/.gnupg/gpg.conf)
-if [ "x$GPG_USE_AGENT" != "x" ]; then
+if [ "x$GPG_USE_AGENT" == "x" ]; then
   echo "use-agent" >> ~/.gnupg/gpg.conf
 fi
 
 if [ ! -f ~/.gnupg/gpg-agent.conf ]; then
   touch ~/.gnupg/gpg-agent.conf
-
-  {
-    echo "default-cache-ttl 0"
-    echo "max-cache-ttl 0"
-    echo "pinentry-program /opt/homebrew/bin/pinentry-touchid"
-  } >> ~/.gnupg/gpg-agent.conf
 fi
+
+{
+  echo "default-cache-ttl 600"
+  echo "max-cache-ttl 600"
+  echo "pinentry-program /opt/homebrew/bin/pinentry-touchid"
+} > ~/.gnupg/gpg-agent.conf
