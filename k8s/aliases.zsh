2b6212bd07ffbfbba9651f4412d0de0dd4af846f
Marcel Dias <marceldiass@gmail.com>
2023-10-01 19:58:15 +0200

    feat: k3d create cluster function
    Signed-off-by: Marcel Dias <marceldiass@gmail.com>

--- k8s/aliases.zsh
+++ k8s/aliases.zsh
@@ -4,3 +4,8 @@ alias ks='kubectl -n kube-system'
 alias kp='kubectl get pods'
 alias kx='kubectx'
 alias kn='kubens'
+
+# create k3d local cluster
+klc() {
+  k3d cluster create -p "8081:80@loadbalancer" --agents 2
+}
