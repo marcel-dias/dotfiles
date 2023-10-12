3019e1418b22f6f701c9a63a78cb33564fb937a9
Marcel Dias <marceldiass@gmail.com>
2023-10-13 00:07:33 +0200

    clean up
    Signed-off-by: Marcel Dias <marceldiass@gmail.com>

--- user/aliases.zsh
+++ user/aliases.zsh
@@ -3,6 +3,5 @@ alias src='cd ~/Code'
 alias blog='cd ~/Code/marceldiass.github.io'
 alias lg='lazygit'
 alias terminal-test="for i in $(seq 1 10); do /usr/bin/time zsh -i -c exit; done"
-alias arm="docker run --rm -it -v ${PWD}:/home/spinnaker armory/arm-cli arm dinghy render dinghyfile"
 alias tf='terraform'
 alias pinentry='pinentry-mac'
