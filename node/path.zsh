ae3b26ddcdc9fa64e1a14ec78de4b6d5846acae2
Marcel Dias <marceldiass@gmail.com>
2024-08-05 19:16:40 +0200

    chore: add nvm source
    Signed-off-by: Marcel Dias <marceldiass@gmail.com>

--- node/path.zsh
+++ node/path.zsh
@@ -2,6 +2,7 @@
 export PATH="$PATH:/usr/local/share/npm/bin:$HOME/.npm/bin"
 export NVM_DIR="$HOME/.nvm"
 export NVM_LAZY_LOAD=true
-export NVM_LAZY_LOAD_EXTRA_COMMANDS=('jflint')
 export NVM_COMPLETION=true
 export NVM_AUTO_USE=true
+
+[[ -a /opt/homebrew/opt/nvm/nvm.sh ]] && source /opt/homebrew/opt/nvm/nvm.sh
