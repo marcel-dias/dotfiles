179ed531d43588dddc579279b963e2dc3eeccc74
Marcel Dias <marceldiass@gmail.com>
2024-08-05 19:16:56 +0200

    chore: add terragrunt alias
    Signed-off-by: Marcel Dias <marceldiass@gmail.com>

--- user/aliases.zsh
+++ user/aliases.zsh
@@ -4,4 +4,5 @@ alias blog='cd ~/Code/marceldiass.github.io'
 alias lg='lazygit'
 alias terminal-test="for i in $(seq 1 10); do /usr/bin/time zsh -i -c exit; done"
 alias tf='terraform'
+alias tg='terragrunt'
 alias pinentry='pinentry-mac'
