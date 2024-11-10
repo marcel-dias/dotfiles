905cd060e0d2c31558cc3b1927fa0c350bdb671b
Marcel Dias <marceldiass@gmail.com>
2024-11-10 21:58:16 +0100

    chore: add k8s alias
    Signed-off-by: Marcel Dias <marceldiass@gmail.com>

--- k8s/aliases.zsh
+++ k8s/aliases.zsh
@@ -3,9 +3,21 @@ alias k='kubectl'
 alias ks='kubectl -n kube-system'
 alias kp='kubectl get pods'
 alias kx='kubectx'
-alias kn='kubens'
+alias kn='k get nodes -o wide'
+alias knl='k get nodes --show-labels'
 
 # create k3d local cluster
 klc() {
   k3d cluster create -p "8081:80@loadbalancer" --agents 2
+}
+
+kroll() {
+  ns="$1"
+  k rollout restart -n $ns $(k get deploy -n $ns -o name )
+}
+
+kdrain() {
+  nodes="$@"
+  echo "nodes==$nodes"
+  k drain --delete-emptydir-data --ignore-daemonsets $nodes
 }
